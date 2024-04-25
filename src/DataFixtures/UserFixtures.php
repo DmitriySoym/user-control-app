d0782c921127063a5070755e97504d6dd7449b58
Dmitriy Soymchenko <d.soymchenko@gmail.com>
2024-04-25 10:54:27 +0300

    feat: add login form

---
+++ src/DataFixtures/UserFixtures.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\DataFixtures;
+
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+use App\Entity\User;
+
+class UserFixtures extends Fixture
+{
+    public function load(ObjectManager $manager): void
+    {
+        // $product = new Product();
+        // $manager->persist($product);
+        $user = new User();
+        $user->setEmail('admin@admin.com');
+        $user->setPassword('$2y$13$IHcOCqHXna0q/2z34ewVDOAK0V0ouzdpbuSCgL/GZpln/AdWhoxIK');
+        $user->setRoles(['ROLE_USER']);
+
+        $manager->persist($user);
+
+        $manager->flush();
+    }
+}
